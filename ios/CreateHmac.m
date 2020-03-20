3b9520fb04f0009fc57d3c1bb81fcd17e5cd2356
Truc <trucdev@gmail.com>
2020-03-20 17:50:47 +0700

    change data to string

--- ios/CreateHmac.m
+++ ios/CreateHmac.m
@@ -6,24 +6,15 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_REMAP_METHOD(createHmacSHA1, secret:(NSString *)secret data:(NSDictionary *)data createHmacWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(createHmacSHA1, secret:(NSString *)secret data:(NSString *)data createHmacWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     resolve([self signData:data secretKey:secret]);
 }
 
-- (NSString*)signData:(NSDictionary *)data
+- (NSString*)signData:(NSString *)data
                                   secretKey: (NSString*)_secretKey {
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:data
-                                                       options:0
-                                                         error:&error];
-    
-    if (!jsonData) {
-        NSLog(@"Got an error: %@", error);
-        return nil;
-    } else {
-        NSString *hash = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        return [hash signHmacSHA1WithKey:_secretKey];
-    }
+    return [data signHmacSHA1WithKey:_secretKey];
 }
 
 @end
+
+
