dc8f20c2facd33b4a678f2a2039f032aa5090540
loicx <xuanloi1408@gmail.com>
2020-03-21 08:54:22 +0700

    add gen sha1 android

--- android/src/main/java/com/reactlibrary/CreateHmacModule.java
+++ android/src/main/java/com/reactlibrary/CreateHmacModule.java
@@ -1,9 +1,18 @@
 package com.reactlibrary;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CreateHmacModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +29,36 @@ public class CreateHmacModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void createHmacSHA1(String key, String message, Promise promise) {
+        try {
+            final String hashingAlgorithm = "HmacSHA1"; //or "HmacSHA1", "HmacSHA512"
+            byte[] bytes = hmac(hashingAlgorithm, key.getBytes(), message.getBytes());
+            final String messageDigest = bytesToHex(bytes);
+            promise.resolve(messageDigest);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            promise.reject(e.getMessage());
+        }
+
+
     }
+
+    public static byte[] hmac(String algorithm, byte[] key, byte[] message) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac mac = Mac.getInstance(algorithm);
+        mac.init(new SecretKeySpec(key, algorithm));
+        return mac.doFinal(message);
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        final char[] hexArray = "0123456789abcdef".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0, v; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 }
